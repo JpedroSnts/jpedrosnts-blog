ab707188c38b35ca2ae19505f93bb2aeb2ce6595
JpedroSnts <jp.siq.snts@gmail.com>
2022-01-19 11:57:44 -0300

    fix api data

--- src/service/blog.ts
+++ src/service/blog.ts
@@ -60,7 +60,7 @@ export async function getPostBySlug(slug: string | string[] | undefined) {
         };
       }) ?? [];
     if (Post) return { post: Post[0] };
-    return { post: [] };
+    return { post: {} };
   }
 }
 
@@ -135,9 +135,9 @@ export function getDisplayData() {
       ],
       text: {
         "pt-BR":
-          "Olá meu nome é João Pedro de Siqueira Santos, tenho 16 anos e atualmente estou cursando técnico em desenvolvimento de sistemas integrado ao ensino médio, ao entrar neste curso a cerca de 1 ano eu não fazia idéia do que iria fazer, e então me apaixonei por este mundo!",
+          "Olá meu nome é João Pedro, tenho 16 anos e atualmente estou cursando técnico em desenvolvimento de sistemas integrado ao ensino médio, ao entrar neste curso a cerca de 1 ano eu não fazia idéia do que iria fazer, e então me apaixonei por este mundo!",
         "en-US":
-          "Hello my name is João Pedro de Siqueira Santos, I'm 16 years old and I'm currently studying a technician in systems development integrated to high school, when I entered this course about 1 year ago I had no idea what I was going to do, and then I fell in love with this world!",
+          "Hello my name is João Pedro, I'm 16 years old and I'm currently studying a technician in systems development integrated to high school, when I entered this course about 1 year ago I had no idea what I was going to do, and then I fell in love with this world!",
       },
     },
   };
