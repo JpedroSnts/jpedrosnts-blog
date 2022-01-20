76bd41851320b019534666c607c49696c320193f
JpedroSnts <jp.siq.snts@gmail.com>
2022-01-20 14:29:45 -0300

    return

--- src/service/blog.ts
+++ src/service/blog.ts
@@ -7,8 +7,7 @@ function getDatePt(date: string) {
   const month =
     dt.getMonth() <= 9 ? `0${dt.getMonth() + 1}` : dt.getMonth() + 1;
   const year = dt.getFullYear();
-  const hours =
-    dt.getHours() - 3 <= 9 ? "0" + (dt.getHours() - 3) : dt.getHours() - 3; // -3 HOURS
+  const hours = dt.getHours() <= 9 ? "0" + dt.getHours() : dt.getHours();
   const minutes =
     dt.getMinutes() <= 9 ? "0" + dt.getMinutes() : dt.getMinutes();
   const date_frm = `${day}/${month}/${year} - ${hours}:${minutes}`;
@@ -20,7 +19,7 @@ function getDateEn(date: string) {
   const day = dt.getDate();
   const month = dt.getMonth() + 1;
   const year = dt.getFullYear();
-  var hours = dt.getHours() - 3; // -3 HOURS
+  var hours = dt.getHours();
   var min = dt.getMinutes();
   var ampm = hours >= 12 ? "PM" : "AM";
   hours = hours % 12;
